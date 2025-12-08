84e079ef9375ac463afdee5fa75684cb5de60fa3
GoyalParas <parasgoyal180803@gmail.com>
2025-12-08 17:03:06 +0530

    Landing page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import {SignedIn, SignedOut, SignOutButton, SignInButton, UserButton} from '@clerk/clerk-react';
+import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
+
+  const { isSignedIn, isLoaded } = useUser();
+
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
+
   return (
-    // <h1 className="text-red-500 bg-orange-400 p-10 text-3xl">Welcome to the app</h1>
+
     <Routes>
+      <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+      <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
       
-      <Route path="/" element={ <HomePage/> } />
       <Route path="/about" element={ <AboutPage/> } />
       
     </Routes>
