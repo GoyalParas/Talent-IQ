563578750a5e92fca34b1b07c8c6f95b101b356f
GoyalParas <parasgoyal180803@gmail.com>
2025-12-10 22:09:06 +0530

    problems page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,11 @@ import {SignedIn, SignedOut, SignOutButton, SignInButton, UserButton} from '@cle
 import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./Pages/HomePage";
-import AboutPage from "./Pages/AboutPage";
+
 import DashboardPage from "./Pages/DashboardPage";
+
+// import ProblemPage from "./pages/ProblemPage";
+import ProblemsPage from "./Pages/ProblemsPage";
 
 function App() {
 
@@ -18,7 +21,7 @@ function App() {
       <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
       <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
       
-      <Route path="/about" element={ <AboutPage/> } />
+      <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
       
     </Routes>
     
